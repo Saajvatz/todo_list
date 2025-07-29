f92b2f1028175cd217282452207e8837516d5065
Saajvatz <159474385+Saajvatz@users.noreply.github.com>
2025-07-29 16:16:21 -0600

    Update headerComponent.js

--- components/header/headerComponent.js
+++ components/header/headerComponent.js
@@ -1,11 +1,10 @@
-export function header(){
-
+export function header() {
     let header = document.createElement('header');
 
     let h1 = document.createElement('h1');
     h1.innerText = " Todo-List";
     header.appendChild(h1);
-    
+
     let divTareas = document.createElement('div');
     divTareas.className = "div-tareas";
 
@@ -18,30 +17,13 @@ export function header(){
     spanNumero.className = "numero-tareas";
     spanNumero.innerText = "8";
     divTareas.appendChild(spanNumero);
+
     header.appendChild(divTareas);
-    
+
     let divLogoUser = document.createElement('div');
     divLogoUser.className = "div-logo-user";
     divLogoUser.innerText = "👤";
     header.appendChild(divLogoUser);
-    
+
     return header;
-
-
-    let header = document.createElement('header')
-    let h1 = document.createElement('h1');
-    h1.innerText = "todo-list";
-    header.appendChild(h1);
-
-    let divTareas = document.createElement("div");
-    divTareas.className = "div-tareas";
-    header.appendChild(divTareas);
-
-    let divLogoUsuario = document.createElement('div');
-    divLogoUsuario.className = "div-logo-usuario";
-    divLogoUsuario.innerText = "🤨";
-    header.appendChild(divLogoUsuario);
-
-    return header
-
 }
